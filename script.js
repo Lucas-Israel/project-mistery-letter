65d5a08c084d4ad3f6e3a3d576975fb67eadcb4b
Lucas Israel <lisraelalves92@gmail.com>
2022-05-30 11:42:02 -0300

    desafios ate o 5

---
+++ script.js
@@ -0,0 +1,29 @@
+const inpCartaTexto = document.querySelector('#carta-texto')
+const btnCriarCarta = document.querySelector('#criar-carta')
+const paragCartaGerada = document.querySelector('#carta-gerada')
+
+
+function checkSpan() {
+  const elem = document.querySelectorAll('span');
+  if (elem) {
+    for (let index = 0; index < elem.length; index++) {
+      elem[index].remove();
+    }
+  }
+}
+
+btnCriarCarta.addEventListener('click', () => {
+  paragCartaGerada.innerText = '';
+  const peguei = inpCartaTexto.value;
+  const usingSplit =  peguei.split(' ');
+  checkSpan();
+  if (inpCartaTexto.value === '' || inpCartaTexto.value === ' ') {
+    paragCartaGerada.innerText = 'Por favor, digite o conteúdo da carta.'
+  } else {
+    for (let index2 = 0; index2 < usingSplit.length; index2 += 1) {
+      const crndSpn = document.createElement('span')
+      crndSpn.innerText = usingSplit[index2];
+      paragCartaGerada.appendChild(crndSpn);
+    }
+  }
+})

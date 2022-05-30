9f2dde9e8c1acbbabd6ac4c636d227efd8c858cd
Lucas Israel <lisraelalves92@gmail.com>
2022-05-30 11:46:37 -0300

    lint

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const inpCartaTexto = document.querySelector('#carta-texto')
-const btnCriarCarta = document.querySelector('#criar-carta')
-const paragCartaGerada = document.querySelector('#carta-gerada')
-
+const inpCartaTexto = document.querySelector('#carta-texto');
+const btnCriarCarta = document.querySelector('#criar-carta');
+const paragCartaGerada = document.querySelector('#carta-gerada');
 
 function checkSpan() {
   const elem = document.querySelectorAll('span');
   if (elem) {
-    for (let index = 0; index < elem.length; index++) {
+    for (let index = 0; index < elem.length; index += 1) {
       elem[index].remove();
     }
   }
@@ -15,15 +14,15 @@ function checkSpan() {
 btnCriarCarta.addEventListener('click', () => {
   paragCartaGerada.innerText = '';
   const peguei = inpCartaTexto.value;
-  const usingSplit =  peguei.split(' ');
+  const usingSplit = peguei.split(' ');
   checkSpan();
   if (inpCartaTexto.value === '' || inpCartaTexto.value === ' ') {
-    paragCartaGerada.innerText = 'Por favor, digite o conteúdo da carta.'
+    paragCartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
   } else {
     for (let index2 = 0; index2 < usingSplit.length; index2 += 1) {
-      const crndSpn = document.createElement('span')
+      const crndSpn = document.createElement('span');
       crndSpn.innerText = usingSplit[index2];
       paragCartaGerada.appendChild(crndSpn);
     }
   }
-})
+});

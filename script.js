cc34df3d2f472b1d06e27c386b64bb2f4067c2a9
Lucas Israel <lisraelalves92@gmail.com>
2022-05-30 12:50:27 -0300

    avancos

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const inpCartaTexto = document.querySelector('#carta-texto');
 const btnCriarCarta = document.querySelector('#criar-carta');
 const paragCartaGerada = document.querySelector('#carta-gerada');
+const estilo = ['newspaper', 'magazine1', 'magazine2'];
+const tamanho = ['medium', 'big', 'reallybig'];
+const rotacao = ['rotateleft', 'rotateright'];
+const inclinacao = ['skewleft', 'skewright'];
 
 function checkSpan() {
   const elem = document.querySelectorAll('span');
@@ -9,6 +13,11 @@ function checkSpan() {
       elem[index].remove();
     }
   }
+}
+
+function rnGzus(arr) {
+  const ΑΩ = Math.round(Math.random() * (arr.length - 1));
+  return arr[ΑΩ];
 }
 
 btnCriarCarta.addEventListener('click', () => {
@@ -22,6 +31,7 @@ btnCriarCarta.addEventListener('click', () => {
     for (let index2 = 0; index2 < usingSplit.length; index2 += 1) {
       const crndSpn = document.createElement('span');
       crndSpn.innerText = usingSplit[index2];
+      crndSpn.classList.add(rnGzus(estilo), rnGzus(tamanho), rnGzus(rotacao), rnGzus(inclinacao));
       paragCartaGerada.appendChild(crndSpn);
     }
   }
